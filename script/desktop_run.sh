7127e20174f0529f8ea248f060dc154329bbd73b
Antony Lewis <antony@wordwebsoftware.com>
2015-06-01 16:29:26 +0100

    working and first public

---
+++ script/desktop_run.sh
@@ -0,0 +1,34 @@
+mkdir /home/ubuntu/.config
+mkdir /home/ubuntu/.config/autostart
+
+cat <<EOT > /home/ubuntu/.config/autostart/startup.desktop
+[Desktop Entry]
+Version=1.0
+Name=StartScripts
+Exec=sh -e /home/ubuntu/scripts/user_config.sh
+Icon=system-run
+Type=Application
+Terminal=false
+EOT
+
+cat <<EOT > /home/ubuntu/.config/autostart/vagrant_terminal.desktop
+[Desktop Entry]
+Version=1.0
+Name=Start terminal
+Type=Application
+Exec=gnome-terminal --working-directory=/vagrant/
+Icon=system-run
+EOT
+
+chown -R ubuntu:ubuntu /home/ubuntu/
+
+#turn off automatic updates
+#cat <<EOT > /etc/apt/apt.conf.d/10periodic
+#APT::Periodic::Update-Package-Lists "0";
+#APT::Periodic::Download-Upgradeable-Packages "0";
+#APT::Periodic::AutocleanInterval "0";
+#EOT
+
+
+
+
